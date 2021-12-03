585c1f83ca0b895559af52d0e01da66fe1425013
Carlos Navarro <carlosarturo28@gmail.com>
2021-12-02 21:14:03 -0500

    adding src

---
+++ src/settings/site.ts
@@ -0,0 +1,60 @@
+import { ROUTES } from '@lib/routes';
+export const siteSettings = {
+  name: 'Zuppli',
+  description: '',
+  logo: {
+    url: '/logo.svg',
+    alt: 'Zuppli',
+    href: '/',
+    width: 128,
+    height: 40,
+  },
+  defaultLanguage: 'en',
+  currencyCode: 'USD',
+  product: {
+    placeholderImage: '/product-placeholder.svg',
+    cardMaps: {
+      grocery: 'Krypton',
+    },
+  },
+  authorizedLinks: [
+    { href: ROUTES.PROFILE, label: 'auth-menu-profile' },
+    { href: ROUTES.ORDERS, label: 'auth-menu-my-orders' },
+    { href: ROUTES.CHECKOUT, label: 'auth-menu-checkout' },
+    { href: ROUTES.LOGOUT, label: 'auth-menu-logout' },
+  ],
+  authorizedLinksMobile: [
+    { href: ROUTES.PROFILE, label: 'auth-menu-profile' },
+    { href: ROUTES.ORDERS, label: 'auth-menu-my-orders' },
+    { href: ROUTES.REFUNDS, label: 'text-my-refunds' },
+    { href: ROUTES.CHECKOUT, label: 'auth-menu-checkout' },
+    { href: ROUTES.CHANGE_PASSWORD, label: 'profile-sidebar-password' },
+    { href: ROUTES.LOGOUT, label: 'auth-menu-logout' },
+  ],
+  dashboardSidebarMenu: [
+    {
+      href: ROUTES.PROFILE,
+      label: 'profile-sidebar-profile',
+    },
+    {
+      href: ROUTES.CHANGE_PASSWORD,
+      label: 'profile-sidebar-password',
+    },
+    {
+      href: ROUTES.ORDERS,
+      label: 'profile-sidebar-orders',
+    },
+    {
+      href: ROUTES.REFUNDS,
+      label: 'text-my-refunds',
+    },
+    {
+      href: ROUTES.HELP,
+      label: 'profile-sidebar-help',
+    },
+    {
+      href: ROUTES.LOGOUT,
+      label: 'profile-sidebar-logout',
+    },
+  ],
+};
