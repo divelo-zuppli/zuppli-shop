93bd9fd375a2472aee88754e7844ad7f9d398e02
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2022-05-23 19:16:51 -0500

    feat: eample of getting categories

--- src/framework/basic-graphql/category/get-all-categories.tsx
+++ src/framework/basic-graphql/category/get-all-categories.tsx
@@ -1,17 +1,20 @@
 import { CategoriesQueryOptionsType, Category } from '@framework/types';
-import http from '@framework/utils/http';
-import { API_ENDPOINTS } from '@framework/utils/api-endpoints';
 import { useQuery } from 'react-query';
+import { API_ENDPOINTS } from '@framework/utils/api-endpoints';
+import categoryService from '../../../services/category.service';
 
 export const fetchCategories = async ({ queryKey }: any) => {
   const [_key, _params] = queryKey;
-  const {
-    data: { data },
-  } = await http.get(API_ENDPOINTS.CATEGORIES);
+  const data = await categoryService.getAll();
+
+  console.log('data: ', data);
+
   return { categories: { data: data as Category[] } };
 };
 
 export const useCategoriesQuery = (options: CategoriesQueryOptionsType) => {
+  console.log('options: ', options);
+
   return useQuery<{ categories: { data: Category[] } }, Error>(
     [API_ENDPOINTS.CATEGORIES, options],
     fetchCategories
